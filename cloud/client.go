31336ee125b349319c90645b08be75285ba52f19
Burak Koken <burakkokenn@gmail.com>
2024-01-09 01:38:48 +0300

    Delete files under cloud package

--- cloud/client.go
+++
@@ -1,7 +0,0 @@
-package cloud
-
-type DiscoveryClient interface {
-	Description() string
-	ServiceInstances(serviceId string) []ServiceInstance
-	Services() []string
-}
--- cloud/registry.go
+++
@@ -1,8 +0,0 @@
-package cloud
-
-type ServiceRegistry interface {
-	Register(instance ServiceInstance)
-	Deregister(instance ServiceInstance)
-	SetStatus(instance ServiceInstance, status string)
-	GetStatus(instance ServiceInstance) any
-}
