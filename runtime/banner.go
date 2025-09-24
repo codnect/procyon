a82dabf9ac54c50d0491a2983e1c82e8e1111c96
Burak Koken <burakkokenn@gmail.com>
2025-09-24 23:13:21 +0300

    refactor(runtime): rename Printer interface to BannerPrinter

--- runtime/banner.go
+++ runtime/banner.go
@@ -16,8 +16,8 @@ package runtime
 
 import "io"
 
-// Banner interface represents a banner that can be printed to an io.Writer.
-type Banner interface {
+// BannerPrinter interface represents a banner that can be printed to an io.Writer.
+type BannerPrinter interface {
 	// PrintBanner method prints the banner to the provided io.Writer.
 	PrintBanner(writer io.Writer) error
 }
